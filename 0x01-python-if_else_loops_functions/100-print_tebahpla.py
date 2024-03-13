350dc44d9542f31a3caa5b078ebd69c488993fef
Soukainaton <soukainaelmouden00@gmail.com>
2024-03-13 10:16:41 -0700

    errors fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 for i in range(25, -1, -1):
     c = i + ord('A')
     if i % 2 == 1:
-        c += 32 
+        c += 23
         print("{:c}".format(c), end="")
