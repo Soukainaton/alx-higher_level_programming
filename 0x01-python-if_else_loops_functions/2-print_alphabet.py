5395a023bc34643718ce26a66b35895f659d2bb2
Soukainaton <soukainaelmouden00@gmail.com>
2024-03-13 07:48:03 -0700

    errors fixed

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/user/bin/pyhton3 
-for i in range(ord('a'), ord('z') +1):
-    print("{:c}.format(i), end="")
+#!/usr/bin/pyhton3 
+for i in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(i), end="")
